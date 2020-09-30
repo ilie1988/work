9205ea919cb7d6c2890d4dcc35df8ba802b10ea0
italo <info@italo.network>
2020-09-30 07:00:20 -0400

    first commit

---
+++ src/hardforks/hardforks.cpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2014-2019, The Monero Project
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "hardforks.h"
+
+#undef MONERO_DEFAULT_LOG_CATEGORY
+#define MONERO_DEFAULT_LOG_CATEGORY "blockchain.hardforks"
+
+const hardfork_t mainnet_hard_forks[] = {
+  { 1, 1, 0, 1541014386, 0 },
+  { 2, 8, 0, 1541014391, 0 },
+  { 3, 10, 0, 1541014463, 0 },
+  { 4, 15, 0, 1549695692, 0 },
+  { 5, 20, 0, 1560481469, 0 },
+  { 6, 25, 0, 1573931994, 0 },
+  { 7, 100, 0, 1595030400, 0 },
+
+};
+const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
+
+const hardfork_t testnet_hard_forks[] = {
+  { 1, 1, 0, 1341378000, 0 },
+
+  { 2, 8, 0, 1445355000, 0 },
+
+  { 3, 100, 0, 1472415034, 0 },
+  { 4, 235, 0, 1472415035, 0 },
+  { 5, 250, 0, 1551499880, 0 },
+  { 6, 300, 0, 1571531327, 0 },
+  { 7, 350, 0, 1581531327, 0 },
+
+};
+const size_t num_testnet_hard_forks = sizeof(testnet_hard_forks) / sizeof(testnet_hard_forks[0]);
+
+const hardfork_t stagenet_hard_forks[] = {
+  { 1, 1, 0, 1341378000, 0 },
+
+};
+const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);
